570b5e3f58bebe59b64e4c6d9920876917d6d131
Tausif <tsftousif2@gmail.com>
2024-01-04 16:26:37 +0800

    Fix routing error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import LoginScreen from "./components/LoginScreen";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
